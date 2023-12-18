34ee34b648cbc2589d070a94b5fe34ac497a8d59
HaekyungSeong <haekyungs95@gmail.com>
2023-12-18 16:44:49 +0900

    특정 조건의 데이터 조회하기

--- src/firebase.js
+++ src/firebase.js
@@ -9,55 +9,11 @@ admin.initializeApp({
 // firestore
 const db = admin.firestore();
 
-// // test-app 컬렉션을 readDb에 담는다
-// const readDb = db.collection("test-app");
-
-// readDb 중 test Doc을 읽어와 getTest에 담는다
-// const getTest = await readDb.doc("test").get();
-
-// const getTest = await readDb.get();
-// getTest.forEach((doc) => {
-//   console.log("문서확인" + doc);
-//   console.log(doc.id, " : ", doc.data());
-// });
-
-// 확인
-// console.log(getTest.data());
-
 // test-app이란 컬렉션에 test 라는 문서 넣기
 const testApp = db.collection("test-app");
 
-// 비동기
-// await testApp.add({
-//   hihi: {
-//     name: "test",
-//     today: "I was car",
-//   },
-//   hehe: {
-//     name: "test2",
-//     today: "I lost my home",
-//   },
-//   haha: {
-//     name: "test3",
-//     today: "I lost my job",
-//   },
-// });
+const selectData = await testApp.where("name", "==", "test").get();
 
-// const data = {
-//   hihi: {
-//     name: "test",
-//     today: "I was car",
-//   },
-//   hehe: {
-//     name: "test2",
-//     today: "I lost my home",
-//   },
-//   haha: {
-//     name: "test3",
-//     today: "I lost my job",
-//   },
-// };
-
-// for (const key in data) {
-//   testApp.doc(key).set(data[key]);
-// }
+selectData.forEach((doc) => {
+  console.log(doc.id, "=>", doc.data());
+});
