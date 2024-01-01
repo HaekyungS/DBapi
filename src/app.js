113ce2c5650c74db2d2e1fd430c00bfca46f8b29
HaekyungSeong <haekyungs95@gmail.com>
2024-01-01 22:37:15 +0900

    api 구성

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { readDB, updateDB } from "./firebase.js";
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -7,14 +8,27 @@ const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static("page"));
+app.use(express.static("src"));
 
 app.get("/", (req, res) => {
   res.send("page read");
 });
 
-// form에 대한 정보수신
-app.post("/edit", urlencodedParser, (req, res) => {
-  // console.log(req.body.msg);
+// DB 추가 및 업데이트
+app.post("/add", urlencodedParser, (req, res) => {
+  let update = updateDB(
+    req.body.collection,
+    req.body.doc,
+    req.body.field,
+    req.body.value,
+    req.body.merge
+  );
+  console.log(update);
+});
+
+// DB 조회
+app.post("/read", urlencodedParser, (req, res) => {
+  readDB(req.body.collection, req.body.doc, req.body.field, req.body.value);
 });
 
 app.listen(2080, () => {
