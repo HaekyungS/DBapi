51802bcb21ebf5ebd19cdd6856c4b8ba27f23149
HaekyungSeong <haekyungs95@gmail.com>
2024-01-01 22:37:28 +0900

    테스트용 form 설계

--- page/index.js
+++ page/index.js
@@ -1,3 +1,4 @@
+// element 생성용 함수
 const elementMaker = (tag, parents, props = {}) => {
   const element = document.createElement(tag);
   Object.assign(element, props);
@@ -5,22 +6,61 @@ const elementMaker = (tag, parents, props = {}) => {
   return element;
 };
 
-const formProps = {
-  method: "post",
-  action: "/edit",
-};
-
-const formTag = elementMaker("form", document.body, formProps);
-
-const inputProps = {
+const collectionInputProps = {
   type: "text",
-  name: "msg",
+  name: "collection",
 };
 
-elementMaker("input", formTag, inputProps);
+const docInputProps = {
+  type: "text",
+  name: "doc",
+};
 
+const fieldInputProps = {
+  type: "text",
+  name: "field",
+};
+
+const valueInputProps = {
+  type: "text",
+  name: "value",
+};
+
+const mergeInputProps = {
+  type: "checkbox",
+  name: "merge",
+};
+
+// 전송
 const submitProps = {
   type: "submit",
 };
 
-elementMaker("input", formTag, submitProps);
+// DB 추가/업데이트용 form
+const addFormProps = {
+  method: "post",
+  action: "/add",
+};
+
+const addFormTag = elementMaker("form", document.body, addFormProps);
+
+elementMaker("input", addFormTag, collectionInputProps);
+elementMaker("input", addFormTag, docInputProps);
+elementMaker("input", addFormTag, fieldInputProps);
+elementMaker("input", addFormTag, valueInputProps);
+elementMaker("input", addFormTag, mergeInputProps);
+elementMaker("input", addFormTag, submitProps);
+
+// DB 조회용 form
+const readFormProps = {
+  method: "post",
+  action: "/read",
+};
+
+const readFormTag = elementMaker("form", document.body, readFormProps);
+
+elementMaker("input", readFormTag, collectionInputProps);
+elementMaker("input", readFormTag, docInputProps);
+elementMaker("input", readFormTag, fieldInputProps);
+elementMaker("input", readFormTag, valueInputProps);
+elementMaker("input", readFormTag, submitProps);
